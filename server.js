e2c013e5224871837feecdc5ef72927936f104ea
Promise Fru <promisefru1@gmail.com>
2020-09-07 08:27:46 +0100

    Project done ..

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected"))
+  .catch(err => console.log(err));
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -25,11 +28,43 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
+var urlSchema = new Schema({
+  url : { type: String }
+})
+
+var Url = mongoose.model("Url", urlSchema);
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.post('/api/shorturl/new', (req, res) => {
+  var postUrl = req.body.url;
+
+  dns.lookup(postUrl, (err) => {
+      if(err) {
+          return res.json({"error":"invalid URL"})
+      }
+
+      var createUrl = new Url({url: postUrl});
+      createUrl
+          .save()
+          .then((doc) => {
+              res.json({"original_url": doc.url, "short_url": doc._id});
+          })
+          .catch(err => console.log(err));
+  })
+})
+
+app.get('/api/shorturl/:identifier', (req, res) => {
+  var identifier = req.params.identifier;
+
+  var findUrl = Url.findById(identifier);
+  findUrl
+      .then((doc) => {
+          if(!doc) return res.sendStatus(404);
+
+          res.redirect('https://' + doc.url);  
+      })
+      .catch(err => console.log(err));
+})
 
 
 app.listen(port, function () {
